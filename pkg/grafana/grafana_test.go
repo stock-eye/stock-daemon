2b67b5b8e2a05ab132ca21c0f357d39c3ac4053f
linclaus <linclaus@outlook.com>
2021-01-31 12:45:12 +0800

    fix bug

--- pkg/grafana/grafana_test.go
+++ pkg/grafana/grafana_test.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"testing"
 
+	"github.com/linclaus/stock-daemon/pkg/kubernetes"
 	"github.com/spf13/viper"
 )
 
 func init() {
 	viper.SetDefault("GRAFANA_AGG_TEMPLATE", "/home/linclaus/work/golang/src/github.com/linclaus/stock-daemon/template/Aggregation.tmpl")
+	viper.SetDefault("K8S_MASTER", "http://127.0.0.1:8001")
+	kubernetes.Init()
 }
 
 func TestAggregation(t *testing.T) {
