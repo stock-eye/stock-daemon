0b943e1c6e2df0ceb1a64dbc12eaff1edad06310
linclaus <linclaus@outlook.com>
2021-03-13 18:32:07 +0800

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ set -x
 CI_COMMIT_TAG=$(git describe --always --tags)
 
 docker build -t linclaus/stock-daemon:${CI_COMMIT_TAG} -f build/package/Dockerfile .
-docker push linclaus/stock-daemon:latest
+docker push linclaus/stock-daemon:${CI_COMMIT_TAG}
